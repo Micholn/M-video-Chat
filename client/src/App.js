ae199fa618c8b620bd56dd5e7c6e0a0d1c8085f9
Inhisowncreed <you@example.com>
2021-07-22 19:51:05 +0100

    saving the background

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,8 @@ import VideoPlayer from './components/VideoPlayer';
 import Notifications  from './components/Notifications';
 import Options from './components/Options';
 
+
+
 const useStyles = makeStyles((theme) => ({
     appBar: {
         borderRadius: 15,
@@ -32,7 +34,9 @@ const useStyles = makeStyles((theme) => ({
 const App = () => {
     const classes = useStyles();
     return ( 
-          <div className={classes.wrapper}>
+          <div style={{ 
+            
+          }} className={classes.wrapper}>
               <AppBar className={classes.appBar} position="static" color="inherit">
                 <Typography variant="h2" align="center">Video chat</Typography> 
               </AppBar>
